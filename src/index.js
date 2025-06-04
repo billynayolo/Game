dc45f2c9a3ccb064e33874aece835f38417030f8
BZ1025 <bkbz3749@gmail.com>
2025-06-04 05:41:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,89 +1,71 @@
-
+html
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <title>bz bk</title>
-    <link rel="icon" type="image/gif" href="res/favicon.ico"/>
+    <link rel="icon" type="image/x-icon" href="res/favicon.ico">
     
-    <meta name="viewport"
-      content="width=device-width,user-scalable=no,initial-scale=1, minimum-scale=1,maximum-scale=1"/>
-      
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="format-detection" content="telephone=no">
     
-    <meta name="renderer" content="webkit"/>
-    <meta name="force-rendering" content="webkit"/>
-    <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1"/>
-    <meta name="ms-application-tap-highlight" content="on">
-    
-    <meta name="full-screen
-          " content="yes"/>
-    <meta name="x5-fullscreen" content="true"/>
-    <meta name="360-fullscreen" content="true"/>
+    <meta name="renderer" content="webkit">
+    <meta name="force-rendering" content="webkit">
+    <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1">
+    <meta name="msapplication-tap-highlight" content="no">
+
+    <meta name="full-screen" content="yes">
+    <meta name="x5-fullscreen" content="true">
+    <meta name="360-fullscreen" content="true">
     
     <meta name="browsermode" content="application">
     <meta name="x5-page-mode" content="app">
-    
-    <style type="text/css">
+
+    <style>
         html {
             -ms-touch-action: none;
         }
-        
         body, canvas, div {
             margin: 0;
             padding: 0;
             outline: none;
-            -moz-user-select: none;
-            -webkit-user-select: none;
-            -ms-user-select: none;
-            khtml-user-select: none;
-            webkit-top-highlight-color: rgba(0,0,0,0);
+            user-select: none;
+-webkit-tap-highlight-color: transparent;
         }
-        
         body {
             position: absolute;
             top: 0;
             left: 0;
             width: 100%;
             height: 100%;
-            padding: 0;
-            border: 0;
-            margin: 0;
-            
-            cursor: default;
             color: #888;
             background-color: #333;
-            
             text-align: center;
-            font-family: helvetica, verdana, arial, sans-serif;
-            
+            font-family: Helvetica, Verdana, Arial, sans-serif;
             display: flex;
             flex-direction: column;
         }
-        
         #cocos2dgamecontainer {
             position: absolute;
             margin: 0;
             overflow: hidden;
-            left: 0px;
-            top: 0px;
-            
+            left: 0;
+            top: 0;
             display: flex;
             align-items: center;
             justify-content: center;
         }
-        
         canvas {
-            background-color: rgba(0, 0, 0, 0);
+            background-color: transparent;
         }
     </style>
 </head>
 <body>
     <script src="res/loading.js"></script>
+    <script src="cocos2d-js-min.js"></script>
+    <script cocos src="main.js"></script>
     <canvas id="gamecanvas" width="480" height="720"></canvas>
 </body>
 </html>
-
-    
